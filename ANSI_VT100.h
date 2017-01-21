bd27eab7df0376eadfd8db678b73ea5e44ba3b35
HighASG936 <lz_rock@hotmail.com>
2017-01-21 13:18:43 -0600

    correción de definiciones

--- ANSI_VT100.h
+++ ANSI_VT100.h
@@ -8,5 +8,4 @@
 const uint8_t  CURSOR_IZQUIERDA[] = {ESC,'[','1','D'};
 const uint8_t  CURSOR_DERECHA  []	= {ESC,'[','C'};
 const uint8_t  LIMPIAR_PANTALLA[]	= {ESC,'[','2','J'};
-const uint8_t	 LIMPIAR_INICIO_LINEA[] = {ESC,'[','1','K'};
 #endif
